55b36b70a3fe49295f9b3bffb6c49e6625ecfd97
jmd5314 <jmd5314@gmail.com>
2024-11-01 05:23:08 +0900

    feat: #10 Application 마이그레이션

---
+++ src/main/java/org/team1/nbe1_3_team01/Nbe13Team01Application.kt
@@ -0,0 +1,12 @@
+package org.team1.nbe1_3_team01
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Nbe13Team01Application 
+
+    fun main(args: Array<String>) {
+        runApplication<Nbe13Team01Application>(*args)
+    }
+
