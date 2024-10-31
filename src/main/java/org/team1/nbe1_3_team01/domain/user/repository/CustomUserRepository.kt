619422c77c6d200723dd58621512690188bcd848
jmd5314 <jmd5314@gmail.com>
2024-10-31 09:36:41 +0900

    feat: #2 레포지토리 마이그레이션

---
+++ src/main/java/org/team1/nbe1_3_team01/domain/user/repository/CustomUserRepository.kt
@@ -0,0 +1,9 @@
+package org.team1.nbe1_3_team01.domain.user.repository
+
+import org.team1.nbe1_3_team01.domain.user.entity.User
+
+interface CustomUserRepository {
+    fun findAllUsersByIdList(userIds: List<Long?>?): List<User?>?
+
+    fun findUsersAndAdminsByCourseId(courseId: Long?): List<User?>?
+}
---
+++ src/main/java/org/team1/nbe1_3_team01/domain/user/repository/CustomUserRepositoryImpl.kt
@@ -0,0 +1,20 @@
+package org.team1.nbe1_3_team01.domain.user.repository
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.team1.nbe1_3_team01.domain.user.entity.QUser
+import org.team1.nbe1_3_team01.domain.user.entity.Role
+import org.team1.nbe1_3_team01.domain.user.entity.User
+
+class CustomUserRepositoryImpl(private val queryFactory: JPAQueryFactory) : CustomUserRepository {
+
+    override fun findAllUsersByIdList(userIds: List<Long?>?): List<User?>? {
+        return queryFactory.selectFrom(QUser.user)
+            .where(QUser.user.id.`in`(userIds))
+            .fetch()
+    }
+
+    override fun findUsersAndAdminsByCourseId(courseId: Long?): List<User?>? {
+        return queryFactory.selectFrom(QUser.user)
+            .where(QUser.user.role.eq(Role.ADMIN).or(QUser.user.course.id.eq(courseId)))
+            .fetch()
+    }
+}
