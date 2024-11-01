4fb9d1ef6f4d26fc74baa692e5c15425728a83ff
power-minu <mw0677@naver.com>
2024-11-01 10:08:36 +0900

    feat: 팀 관련 레포지토리 마이그레이션

---
+++ src/main/java/org/team1/nbe1_3_team01/domain/group/repository/TeamRepository.kt
@@ -0,0 +1,29 @@
+package org.team1.nbe1_3_team01.domain.group.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.team1.nbe1_3_team01.domain.group.entity.Team
+import java.util.*
+
+interface TeamRepository : JpaRepository<Team?, Long?> {
+    fun findByCreationWaiting(waiting: Boolean): List<Team>
+
+    fun deleteById(id: Long)
+
+    @Query(
+        "select t " +
+                "from Team t " +
+                "join fetch t.belongings b " +
+                "where b.isOwner = true and t.id = :teamId"
+    )
+    fun findByIdWithLeaderBelonging(@Param("teamId") teamId: Long): Team?
+
+    @Query(
+        "select t " +
+                "from Team t " +
+                "join fetch t.course c " +
+                "where c.id = :courseId"
+    )
+    fun findByCourseId(@Param("courseId") courseId: Long): List<Team>
+}
