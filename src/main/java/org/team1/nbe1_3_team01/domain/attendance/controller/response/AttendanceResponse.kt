f3919c25ff8175ff247a7aa32adcf9f811ec4823
sangcci <pshsh910@gmail.com>
2024-11-04 17:52:47 +0900

    fix: Response에 ApprovalState 상태 추가

--- src/main/java/org/team1/nbe1_3_team01/domain/attendance/controller/response/AttendanceResponse.kt
+++ src/main/java/org/team1/nbe1_3_team01/domain/attendance/controller/response/AttendanceResponse.kt
@@ -1,5 +1,6 @@
 package org.team1.nbe1_3_team01.domain.attendance.controller.response
 
+import org.team1.nbe1_3_team01.domain.attendance.entity.ApprovalState
 import org.team1.nbe1_3_team01.domain.attendance.entity.IssueType
 import java.time.LocalDateTime
 
@@ -7,6 +8,7 @@ data class AttendanceResponse(
     val attendanceId: Long,
     val userId: Long,
     val username: String,
+    val state: ApprovalState,
     val issueType: IssueType,
     val startAt: LocalDateTime,
     val endAt: LocalDateTime,
