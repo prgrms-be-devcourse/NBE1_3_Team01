2b32a5fd25699dc26d318c422b1e9b4e7b76b570
sangcci <pshsh910@gmail.com>
2024-11-04 10:04:57 +0900

    refactor: config kotlin migration

--- src/main/java/org/team1/nbe1_3_team01/global/config/WebConfig.kt
+++ src/main/java/org/team1/nbe1_3_team01/global/config/WebConfig.kt
@@ -1,19 +1,15 @@
-package org.team1.nbe1_3_team01.global.config;
+package org.team1.nbe1_3_team01.global.config
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.team1.nbe1_3_team01.global.security.AuthInterceptor;
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.team1.nbe1_3_team01.global.security.AuthInterceptor
 
 @Configuration
-@RequiredArgsConstructor
-public class WebConfig implements WebMvcConfigurer {
-
-    private final AuthInterceptor authInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authInterceptor);
+class WebConfig(
+    private val authInterceptor: AuthInterceptor
+) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(authInterceptor)
     }
 }
--- src/main/java/org/team1/nbe1_3_team01/global/config/WebSocketConfig.kt
+++ src/main/java/org/team1/nbe1_3_team01/global/config/WebSocketConfig.kt
@@ -1,26 +1,21 @@
-package org.team1.nbe1_3_team01.global.config;
+package org.team1.nbe1_3_team01.global.config
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-
-
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic"); // 메세지의 브로커를 설정(수신)
-        config.setApplicationDestinationPrefixes("/app"); // 애플리케이션의 목적지 접두사(발행)
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+    override fun configureMessageBroker(config: MessageBrokerRegistry) {
+        config.enableSimpleBroker("/topic") // 메세지의 브로커를 설정(수신)
+        config.setApplicationDestinationPrefixes("/app") // 애플리케이션의 목적지 접두사(발행)
     }
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/stomp-endpoint") // 웹소켓 엔드포인트 설정
-                .setAllowedOriginPatterns("*"); // 일단 모든 오리진 허용
+            .setAllowedOriginPatterns("*") // 일단 모든 오리진 허용
     }
 }
